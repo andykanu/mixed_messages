5e3dd1a73299a6b708c5100f068b2a190dd22950
andykanu <onelittlebear@gmail.com>
2021-01-18 23:10:15 +0000

    using local arrays to create message

--- message_generator.js
+++ message_generator.js
@@ -1,30 +1,27 @@
-//retrieve quote of the day from 'They Said So' endpoint
-const endpoint = "https://quotes.rest/qod?category=inspire";
+//declare array of random sentence parts
+const first = ["In sooth, ", "To be or not to be, ", "All together now, " , "When you're on your own ", "With my shiny new star ", "We can turn blank white ", "Hospital food "];
+const second = ["I know not why ", "that is the question, ", "all together now, ", "in this life " ,"tennis shoes on ", "in a blank white house ", "and I'll write your name "];
+const third = ["I am so sad.", "fabulous destiny", "in no man's land.", "and the days and nights are long.", "I'm afraid of everyone", "popular culture wars.", "are you trying to seduce me?"];
+//function to select string from array
+const section = arr => arr[Math.floor(Math.random()* arr.length)];
 
-async function retrieveQOD () {
-    try { 
-        const response = await fetch(endpoint);
-        if(!response.ok) {
-            throw Error(response.statusText)
-        }
-        const json = await response.json()
+//call function to create sections
 
-         let quote = json.contents.quotes[0].quote;
-         let author = json.contents.quotes[0].author;
-         console.log(quote);
-         console.log(author);
-         return quote, author;
-      }
-    catch (err) {
-        console.log(err);
-        alert('Failed to retrieve quote');
-    }
-}
+const createSentence = () => {
+    const firstSection = section(first);
+    const secondSection = section(second);
+    const thirdSection = section(third);
+    return `${firstSection}${secondSection}${thirdSection}`
+};
+//event listener and call event for button
+const msgBtn = document.getElementById('get_message');
+msgBtn.addEventListener('click', displayMessage);
 
-const displayMessage = (quote, author) => {
-     display_quote = document.getElementById('quote');
-     display_quote.style.display = 'block';
-     display_quote.textContent = quote;
-     console.log(quote);
-     console.log(author);
-} 
+//display quote when button clicked
+function displayMessage () {
+    msgBtn.innerHTML = 'Try again'
+    const quoteDisplay = document.getElementById('quote');
+    quoteDisplay.style.display = "block";
+    quoteDisplay.innerHTML = createSentence();
+    document.getElementById('instructions').style.display = 'none';
+};
